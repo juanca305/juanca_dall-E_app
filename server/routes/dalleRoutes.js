4dc7393a1928ee016554d8433264120974f132a6
Juan Carlos <jc.rdguez64@gmail.com>
2025-02-20 00:21:49 -0500

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -38,7 +38,7 @@ router.route('/').post(async (req, res) => {
             response_format: 'b64_json'
         })
             
-        const image = aiResponse?.data[0].b64_json
+        const image = aiResponse?.data.data[0].b64_json
         res.setHeader('Access-Control-Allow-Origin', '*');
         
         res.status(200).json({ photo: image })
