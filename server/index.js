0fca2130ce32d909a93aa99a1ec8facbee168cb2
Juan Carlos <jc.rdguez64@gmail.com>
2025-02-20 00:04:54 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,21 +17,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-///********* NEW CODE *******////////
-/*const whiteList = ['https://juancadall-e.com'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not Allowed by Cors'))
-    }
-  },
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-//******END NEW CODE******//
-
 app.use(cors({
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -64,54 +49,7 @@ const startServer = async () => {
 }
 startServer();
 
-//************************************************************************************
-/*import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
 
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
-
-dotenv.config();
-
-const app = express();
-
-/*const whiteList = ['https://juancadall-e.com'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not Allowed by Cors'))
-    }
-  },
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));*/
-
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
-
-app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'Hello from DALL.E!',
-  });
-});
-
-const startServer = async () => {
-  try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-startServer();*/
 
 
 
