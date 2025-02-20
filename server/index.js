dff11a4962f90e8c8feb704e3c31f4407ae2c902
Juan Carlos <jc.rdguez64@gmail.com>
2025-02-20 00:39:24 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 //Initialize express app.
 const app = express();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://juancadall-e.com/");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
