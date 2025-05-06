97d3285ddd0579d0d8b0a8372c2d73f8c8a1660a
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2025-05-06 07:49:55 -0400

    Changed struct of response on image

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -87,8 +87,8 @@ router.route('/').post(async (req, res) => {
 
       console.log("🟢 Prompt being sent to OpenAI:", prompt);
 
-      ///const image = aiResponse?.data?.data?.[0]?.b64_json;
-      const image = aiResponse?.data?.[0].b64_json;
+      const image = aiResponse?.data?.data?.[0]?.b64_json;
+      //const image = aiResponse?.data?.[0].b64_json;
       res.status(200).json({ photo: image });
     } catch (error) {
         console.error('🔥 OpenAI error caught:\n', error);
