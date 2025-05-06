61722ec4293d06b0dd26c2fc26ff7957bd6ad85c
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2025-05-06 09:33:16 -0400

    Nullable data

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -138,7 +138,7 @@ router.route('/').post(async (req, res) => {
       response_format: 'b64_json',
     });
 
-    const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse?.data?.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
