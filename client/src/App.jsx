e57967a1754d1d9a4e65f56722ffbb1d7be70588
Juan Carlos <jc.rdguez64@gmail.com>
2025-05-07 15:55:15 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to="/">
           <img src={logo} alt='logo' className='w-28 object-contain'/>
-        </Link>
+        </Link><br />
         <p>by: juanca 2025</p>
         <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
           Create
