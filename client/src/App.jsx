f4a9a454f91885dd314f68125239c1913cfed4c0
Juan Carlos <jc.rdguez64@gmail.com>
2025-05-07 15:47:26 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
         </Link>
         <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
           Create
-        </Link></br>
+        </Link>
       <p>by: juanca 2025</p>
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
