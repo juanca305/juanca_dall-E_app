55e5851f0d4737e0c49656e681353d4286120805
Juan Carlos <jc.rdguez64@gmail.com>
2025-05-07 15:53:15 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,10 +14,11 @@ const App = () => {
         <Link to="/">
           <img src={logo} alt='logo' className='w-28 object-contain'/>
         </Link>
+        <p>by: juanca 2025</p>
         <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
           Create
         </Link>
-      <p>by: juanca 2025</p>
+      
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
         <Routes>
